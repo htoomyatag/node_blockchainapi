a5b04f2f94a43944a8dc1fb60fba3123a11b6957
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2022-04-18 11:57:50 +0630

    yags added with switch case condtions

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var copyFrom = require('pg-copy-streams').from
 var fs = require('fs')
 var async = require('async')
 var { Pool } = require('pg')
+const yargs = require('yargs')
+const args = require('yargs').argv;
 const { config } = require("./config");
 
 
@@ -15,55 +17,95 @@ const { config } = require("./config");
 
    
     // create database
-    pool.query("CREATE DATABASE" +" "+ config.database + ";", (err) => {
+    // pool.query("CREATE DATABASE" +" "+ config.database + ";", (err) => {
 
 
-         if (err) {
-              console.log(err.message);
-        } else {
+    //      if (err) {
+    //           console.log(err.message);
+    //     } else {
 
-                 var conString = `postgres://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`
-        var client = new pool.Client(conString);
+    //              var conString = `postgres://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`
+    //     var client = new pool.Client(conString);
     
-        client.connect(function(err) {
+    //     client.connect(function(err) {
 
-            // create table
-            client.query("CREATE TABLE transaction(timestamp BIGINT,transaction_type VARCHAR,token VARCHAR,amount DECIMAL)", (err, res) => {
-            // copy data to table
-            var stream = client.query(copyFrom("COPY transaction (timestamp,transaction_type,token,amount) FROM STDIN CSV HEADER"));
+    //         // create table
+    //         client.query("CREATE TABLE transaction(timestamp BIGINT,transaction_type VARCHAR,token VARCHAR,amount DECIMAL)", (err, res) => {
+    //         // copy data to table
+    //         var stream = client.query(copyFrom("COPY transaction (timestamp,transaction_type,token,amount) FROM STDIN CSV HEADER"));
           
-               return new Promise(async (resolve, reject) => {
+    //            return new Promise(async (resolve, reject) => {
 
-                    var fileStream = fs.createReadStream('../transactions.csv')
-                    fileStream.pipe(stream);
+    //                 var fileStream = fs.createReadStream('../transactions.csv')
+    //                 fileStream.pipe(stream);
 
-                })
+    //             })
 
                     
-            });
-        });
-        }
+    //         });
+    //     });
+    //     }
         
        
 
-    });
-
-
-  
-  
-
-    // pool.query("CREATE DATABASE" +" "+ config.database + ";", (res,err) => {
-        
-    //    try {
-    //          if(res) {
-    //             console.log('success')
-    //          }
-                
-          
-    //    }
-
-    //    catch (err) {
-    //          console.log('err')
-    //    }
-
     // });
+
+
+
+
+
+
+
+
+yargs.command({
+    command: 'GetPortfolioBy',
+    handler: ()=>{
+     
+        switch (true) {
+
+
+            case args.token !== undefined && args.date === undefined:
+                console.log('question2');
+                break;
+
+            case args.date !== undefined && args.token === undefined:
+                console.log('question3');
+                break;
+
+
+            case args.token !== undefined && args.date !== undefined:
+                console.log('question4');
+                break;
+
+            default:
+               console.log('question1');
+               break;
+
+      
+                        
+        }
+
+
+
+    }
+})
+
+
+yargs.parse()
+
+
+
+
+
+
+// main.js 
+
+
+// main.js aok --token=btc
+// main.js aok --date=12-06-1993
+// main.js aok --date=12-06-1993 --token=btc
+
+
+
+
+
